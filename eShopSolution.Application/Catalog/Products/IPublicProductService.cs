e71d9d714e132dabaee7afc6ba8818b642e5f7ed
Namtc2 <126969945+Namtc2@users.noreply.github.com>
2023-03-25 22:47:17 +0700

    Create Viewmodel and Service for product in Application

---
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Common;
+using eShopSolution.ViewModels.Catalog.Products.Public;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetProductPagingRequest request);
+    }
+}
